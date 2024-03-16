32ef45e91e6ca70f35b0e2c502572f0dac268272
Paul Theby <paultheby@gmail.com>
2024-03-16 11:50:24 -0500

    running rails/redis/sidekiq servers
    Also added routes to observe sidekiq in action in the UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  require "sidekiq/web"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  Rails.application.routes.draw do
+    Sidekiq::Web.use ActionDispatch::Cookies
+    Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+    mount Sidekiq::Web, at: "/sidekiq"
+  end
 end
